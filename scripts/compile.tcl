9b5a41bd634e36768ca7b2e97a94771a50159ad6
Martin-N-Menendez <menendez91@live.com.ar>
2024-09-10 22:14:09 +1000

    Update compile.tcl

--- scripts/compile.tcl
+++ scripts/compile.tcl
@@ -1,1 +1,7 @@
-
+# Compile the VHDL files
+vlib work
+vcom -2008 path/to/module1.vhd
+vcom -2008 path/to/module2.vhd
+vcom -2008 path/to/module3.vhd
+vcom -2008 path/to/module4.vhd
+vcom -2008 path/to/module5.vhd
