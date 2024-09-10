e9b6376b6401743a21ce3aa0c8113f9c27a1dca7
Martin-N-Menendez <menendez91@live.com.ar>
2024-09-10 22:45:24 +1000

    Update module1.vhd

--- source/module1.vhd
+++ source/module1.vhd
@@ -1,1 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
+entity module1 is
+    Port ( A : in  STD_LOGIC_VECTOR (3 downto 0);
+           B : in  STD_LOGIC_VECTOR (3 downto 0);
+           SUM : out  STD_LOGIC_VECTOR (3 downto 0));
+end module1;
+
+architecture Behavioral of module1 is
+begin
+    SUM <= A + B;
+end Behavioral;
